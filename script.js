e07ab251d566c659ea6d0a7fe88362dd84f87dd2
Abhirishi <54245560+Abhirishi@users.noreply.github.com>
2021-02-10 13:08:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -421,31 +421,6 @@ function cellIsAWall(i, j, cells){
 	return $(cells[cellNum]).hasClass("wall");
 }
 
-// DFS
-function DFS(i, j, visited){
-	if (i == endCell[0] && j == endCell[1]){
-		cellsToAnimate.push( [[i, j], "success"] );
-		return true;
-	}
-	visited[i][j] = true;
-	cellsToAnimate.push( [[i, j], "searching"] );
-	var neighbors = getNeighbors(i, j);
-	for(var k = 0; k < neighbors.length; k++){
-		var m = neighbors[k][0];
-		var n = neighbors[k][1]; 
-		if ( !visited[m][n] ){
-			var pathFound = DFS(m, n, visited);
-			if ( pathFound ){
-				cellsToAnimate.push( [[i, j], "success"] );
-				return true;
-			} 
-		}
-	}
-	cellsToAnimate.push( [[i, j], "visited"] );
-	return false;
-}
-
-
 // BFS
 function BFS(){
 	var pathFound = false;
